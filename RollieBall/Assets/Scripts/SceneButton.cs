3b8df318a362d9c630a3fbc13200949182eb178f
ClarenceE3 <90079900+ClarenceE3@users.noreply.github.com>
2022-06-01 02:52:40 -0400

    Menu UI button

---
+++ RollieBall/Assets/Scripts/SceneButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class SceneButton : MonoBehaviour
+{
+    public Button theButton;
+    void Start () {
+		Button btn = theButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		//Debug.Log ("You have clicked the button!");
+		SceneManager.LoadScene("Play Scene");
+	}
+
+
+}
+
+
