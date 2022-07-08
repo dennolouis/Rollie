719622607cf1e665ffdfc9f80ad089bdc27d28cb
ClarenceE3 <90079900+ClarenceE3@users.noreply.github.com>
2022-07-08 01:41:15 -0400

    Loadscene next

--- RollieBall/Assets/Scripts/Collide.cs
+++ RollieBall/Assets/Scripts/Collide.cs
@@ -1,14 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Collide : MonoBehaviour
 {
+  public GameObject GameOver;
+  public bool nowOver;
+
   void OnCollisionEnter (Collision collision) 
   {      
     if (collision.gameObject.tag == "Obstacle" ) 
     {
-      Debug.Log("Heyyyy");
+      GameOver.SetActive(true);
+      GameIsOver();
     } 
+  }
+
+  public void  GameIsOver(){
+    
+    Debug.Log("Game Over!!!");
   }
 }
